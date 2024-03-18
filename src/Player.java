470a0bf6031412d4614cee3dc8f2b2df606ebb80
Boootyshaker9000 <zreli99@gmail.com>
2024-03-18 10:40:39 +0100

    Created Playing_field and Player class

---
+++ src/Player.java
@@ -0,0 +1,7 @@
+public class Player {
+    private char player_shape;
+
+    public Player(char player_shape) {
+        this.player_shape = player_shape;
+    }
+}
---
+++ src/Playing_field.java
@@ -0,0 +1,7 @@
+public class Playing_field {
+    private String[][] playing_field;
+
+    public Playing_field(String[][] playing_field) {
+        this.playing_field = playing_field;
+    }
+}
