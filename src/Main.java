dbfd3ad99e51f7826d808ce151c08f5fea2b9b1d
Boootyshaker9000 <zreli99@gmail.com>
2024-03-18 10:49:09 +0100

    Changed playing field from String[][] to char[][]

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Playing_field playing_field = new Playing_field(new char[3][3]);
     }
 }
--- src/Playing_field.java
+++ src/Playing_field.java
@@ -1,7 +1,7 @@
 public class Playing_field {
-    private String[][] playing_field;
+    private char[][] playing_field;
 
-    public Playing_field(String[][] playing_field) {
+    public Playing_field(char[][] playing_field) {
         this.playing_field = playing_field;
     }
 }
